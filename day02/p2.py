9e5f724325296bbea75d74a00a47701a37efef4e
31b4 <szilagyibencee@gmail.com>
2024-12-02 11:58:55 +0100

    better day2 p2

--- day02/p2.py
+++ day02/p2.py
@@ -2,4 +2,4 @@ l = [list(map(int,x.split())) for x in open("i.txt")]
 
 def isSafe(s): return any(all(d<b-a<u for a,b in zip(s,s[1:])) for d,u in[(0,4),(-4,0)])
 
-print(sum(isSafe(s) or any(isSafe(s[:i]+s[i+1:]) for i in range(len(s))) for s in l))
+print(sum(any(isSafe(s[:i]+s[i+1:]) for i in range(len(s))) for s in l))
