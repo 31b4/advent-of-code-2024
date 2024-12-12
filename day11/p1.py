79e2779295b12c0cd989b246bfce3165755c924c
31b4 <szilagyibencee@gmail.com>
2024-12-12 20:09:52 +0000

    day11 late

---
+++ day11/p1.py
@@ -0,0 +1,20 @@
+def rules(n):
+    if n == 0:
+        return [1]
+    s = str(n)
+    if len(s) % 2 == 0:
+        l = len(s) // 2
+        return [int(s[:l]), int(s[l:])]
+    return [n * 2024]
+
+def calc(stone, blinks):
+    if blinks == 0:
+        return 1
+    new = rules(stone)
+    return sum(calc(num, blinks - 1) for num in new)
+
+res = 0
+stones = list(map(int, open('i.txt').read().split()))
+for stone in stones:
+    res += calc(stone, 25)
+print(res)
